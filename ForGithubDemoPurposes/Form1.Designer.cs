ea6f2cb1e956f259eed2ee418154340b5fcf05e5
KhaylStud <mikhjnelo@gmail.com>
2025-05-27 14:41:10 +0800

    Updated one

--- ForGithubDemoPurposes/Form1.Designer.cs
+++ ForGithubDemoPurposes/Form1.Designer.cs
@@ -85,7 +85,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(62, 199);
+            this.listBox1.Location = new System.Drawing.Point(65, 229);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 84);
             this.listBox1.TabIndex = 8;
